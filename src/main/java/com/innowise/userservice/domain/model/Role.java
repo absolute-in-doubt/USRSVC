be2bd81005ee87d9f7ca94f010e1f57aeb70b369
AbsoluteInDoubt <pervysbor@gmail.com>
2026-06-04 20:22:55 +0300

    fix: moved Roles to the domain layer

---
+++ src/main/java/com/innowise/userservice/domain/model/Role.java
@@ -0,0 +1,7 @@
+package com.innowise.userservice.domain.model;
+
+public enum Role {
+    USER,
+    ADMIN,
+    SERVICE
+}
--- src/main/java/com/innowise/userservice/infrastructure/security/model/Role.java
+++
@@ -1,7 +0,0 @@
-package com.innowise.userservice.infrastructure.security.model;
-
-public enum Role {
-    USER,
-    ADMIN,
-    SERVICE
-}
